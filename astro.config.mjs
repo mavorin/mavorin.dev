da9a93ac6945c95f4ab96abf475c5b626297b885
Marcus Vinicius Salvador <marcus.v.salvador92@protonmail.com>
2026-04-21 19:44:25 -0300

    feat: add letter theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ export default defineConfig({
     prefetchAll: true,
   },
   // !IMPORTANT: Set site url property with your own domain
-  site: 'https://riceball-tw.github.io',
+  site: 'https://mavorin.dev',
   build: {
     inlineStylesheets: 'always',
   },
